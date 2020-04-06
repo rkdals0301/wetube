4490a0b485ad836cba23cee19dd80c4fda784ef1
rkdals0301 <rkdals0301@naver.com>
2020-04-07 03:14:53 +0900

    1. import es6 문법으로 변경 2. babel의 수정이 일어나서 nodemon 2번 동작 수정 3. middleware 전역 사용

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,6 +9,13 @@ const handleListening = () => console.log(`Listening on: http//localhost:${PORT}
 const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
