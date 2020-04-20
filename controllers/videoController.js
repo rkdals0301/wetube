1199dc66ee4a32232919bfb0be65dfc4bbc5f482
rkdals0301 <rkdals0301@naver.com>
2020-04-21 01:26:00 +0900

    Log In And User Profile Controller 작업

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,6 +7,6 @@ export const search = (req, res) => {
 };
 export const videos = (req, res) => res.render("videos", { pageTitle : "Videos" });
 export const upload = (req, res) => res.render("upload", { pageTitle : "Upload" });
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "VideoDetail" });
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "EditVideo" });
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : "DeleteVideo" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : "Delete Video" });
